61a2bb461d26d45c4ba4a10b1d70c4cf7b495707
xgenetiicz <gentiagron123@gmail.com>
2026-09-16 13:03:40 +0200

    feat:
    Added full changes on ContentController.java with correct fields and constructor. Also added the methods now from projectController, and referred those to ContentService

--- src/main/java/com/example/genetiicz/Controller/ContentController.java
+++ src/main/java/com/example/genetiicz/Controller/ContentController.java
@@ -1,4 +1,67 @@
 package com.example.genetiicz.Controller;
 
+
+import com.example.genetiicz.DTO.ContentDTO;
+import com.example.genetiicz.Entity.UserEntity;
+import com.example.genetiicz.Repository.ContentRepository;
+import com.example.genetiicz.Repository.ProjectRepository;
+import com.example.genetiicz.Repository.UserRepository;
+import com.example.genetiicz.Service.ContentService;
+import com.example.genetiicz.Service.ProjectService;
+import com.example.genetiicz.Service.UserService;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
 public class ContentController {
+
+    //private ProjectService projectService;
+    private ContentService contentService;
+    private UserRepository userRepository;
+
+    public ContentController(ContentService contentService,
+                             UserRepository userRepository) {
+        this.contentService = contentService;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/upload/image/{projectId}")
+    public ResponseEntity<String> uploadProjectImage(@PathVariable Long projectId, @RequestParam("file") MultipartFile file) throws FileUploadException {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName(); // we reuse the securitycontext since
+        //only auth users can upload image to their projects.
+        UserEntity user = userRepository.findByEmail(email).get(); //so i fetch the user and find it by email where this checks for auth,
+
+        //and then store this in a Long datatype with the reference userId and point this to the fetched user.getUserId();
+        //userId has the actual userId, this could have been fixed in another way, maybe just pointing to the email instead, since it is unique
+        //and the email could have several projects, and the authenticationManager points to the email for auth credentials.
+        Long userId = user.getUserId();
+        String result = contentService.uploadProjectImage(projectId,userId,file);
+        return ResponseEntity.status(201).body("Image uploaded successfully with filename: " + result);
+    }
+
+    @PostMapping("upload/files/{projectId}")
+    public ResponseEntity<List<String>>uploadFilesIntoProject(@PathVariable Long projectId, @RequestParam("content")List<MultipartFile> differentFiles) throws FileUploadException {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        List<String> savedPaths = contentService.uploadFilesIntoProject(projectId,email,differentFiles);
+        return ResponseEntity.status(201).body(savedPaths);
+    }
+
+    @GetMapping("/content/{projectId}")
+    public ResponseEntity<List<ContentDTO>> getContentForProject(@PathVariable Long projectId) {
+        List<ContentDTO> content = contentService.getContentForProject(projectId);
+        return ResponseEntity.ok(content);
+    }
+
+    @DeleteMapping("/delete/{projectId}/{contentId}")
+    public ResponseEntity<String>deleteContent(@PathVariable Long projectId, @PathVariable Long contentId) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        contentService.deleteContent(projectId,contentId,email);
+        return ResponseEntity.ok("Content deleted successfully");
+    }
 }
