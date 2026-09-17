6d18f72adc52f861beea8cee373d5e955353bd06
xgenetiicz <gentiagron123@gmail.com>
2026-09-17 21:19:22 +0200

    feat & rework: Changed getAllProjects() - it is now possible to retrieve all contents pointed to project.
    - private List<ContentDTO> content; --> is added as a field in ProjectDTO.java.
    
    There are two loops with arraylists, and for enhanced loops. one iterates over the list in Project and other in Content.

--- src/main/java/com/example/genetiicz/DTO/ProjectDTO.java
+++ src/main/java/com/example/genetiicz/DTO/ProjectDTO.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
@@ -35,6 +36,9 @@ public class ProjectDTO {
     private LocalDate endDate;
 
     private String imagePath;
+
+    //List for retrieving all contents within the project.
+    private List<ContentDTO> content;
 
 
 
