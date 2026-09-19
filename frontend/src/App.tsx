c3b5c4942dd3fc213b66df0325a611d78d69f422
xgenetiicz <gentiagron123@gmail.com>
2026-09-19 13:37:08 +0200

    fix: typo in route path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/projects" element={<ProjectPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
