6925eb4c40405910d80ad832b0c629884bb7a455
xgenetiicz <gentiagron123@gmail.com>
2026-09-19 18:47:49 +0200

    feat: updated client.ts
    - intercept jwt token request and get bearer token.

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -4,4 +4,12 @@ const client = axios.create({
   baseURL: "http://localhost:8080/api",
 });
 
+client.interceptors.request.use((config) => {
+  const token = localStorage.getItem("buildhub_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default client;
