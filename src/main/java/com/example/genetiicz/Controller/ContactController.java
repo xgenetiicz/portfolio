e673f0d13dc3eeb1bdcbf5c127b8b19de66b9baf
xgenetiicz <gentiagron123@gmail.com>
2026-09-19 16:30:48 +0200

    fix: @Valid annotiation on /contact controller.
    - Validates annotations now in ContactFormDTO

--- src/main/java/com/example/genetiicz/Controller/ContactController.java
+++ src/main/java/com/example/genetiicz/Controller/ContactController.java
@@ -3,6 +3,7 @@ package com.example.genetiicz.Controller;
 import com.example.genetiicz.DTO.ContactFormDTO;
 import com.example.genetiicz.Service.EmailService;
 import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +20,9 @@ public class ContactController {
     }
 
     @PostMapping("/contact")
-    public ResponseEntity <String> contactFormWithTopic(@RequestBody ContactFormDTO contactFormDTO,String subject) throws MessagingException {
+    public ResponseEntity <String> contactFormWithTopic(@Valid @RequestBody ContactFormDTO contactFormDTO, String subject) throws MessagingException {
         String emailSent = emailService.contactFormWithTopic(contactFormDTO,subject);
+
         if(emailSent.isBlank()) {
             return ResponseEntity.status(404).body("Email is not sent, and cannot reach the destination");
         } else {
