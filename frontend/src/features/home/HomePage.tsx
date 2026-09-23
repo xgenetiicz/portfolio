e93a3dc8c0b029acb8064612372a6b67af9b1e35
xgenetiicz <gentiagron123@gmail.com>
2026-09-23 20:44:35 +0200

    Added FeaturedProjects to HomePage.tsx

--- frontend/src/features/home/HomePage.tsx
+++ frontend/src/features/home/HomePage.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "./AboutSection";
 import TerminalIntro from "./TerminalIntro";
+import FeaturedProjects from "./FeaturedProjects";
 
 export default function HomePage() {
   return (
     <main>
       <TerminalIntro />
       <AboutSection />
+      <FeaturedProjects />
     </main>
   );
 }
